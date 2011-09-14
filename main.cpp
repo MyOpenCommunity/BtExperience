dab442554053d1bd6361aa21112cec9f32d7154c
Gianni Valdambrini <aleister@develer.com>
2011-09-14 15:22:53 +0200

    adesso utilizza il vecchio paint engine di qt, con il quale l'antialiasing funziona correttamente (su desktop, su omap per il momento no)

--- main.cpp
+++ main.cpp
@@ -5,14 +5,20 @@
 
 int main(int argc, char *argv[])
 {
+    qDebug("Paint engine opengl 1!");
+    QGL::setPreferredPaintEngine(QPaintEngine::OpenGL);
     QApplication app(argc, argv);
 
     QmlApplicationViewer viewer;
     viewer.setViewport(new QGLWidget);
     viewer.setViewportUpdateMode(QGraphicsView::FullViewportUpdate);
+    viewer.setRenderHint(QPainter::Antialiasing, true);
+    viewer.setRenderHint(QPainter::SmoothPixmapTransform, true);
+    viewer.setRenderHint(QPainter::HighQualityAntialiasing, true);
+    viewer.setRenderHint(QPainter::TextAntialiasing, true);
 
     viewer.setOrientation(QmlApplicationViewer::ScreenOrientationAuto);
-        viewer.setMainQmlFile(QLatin1String("qml/bt_experience/main.qml"));
+    viewer.setMainQmlFile(QLatin1String("qml/bt_experience/main.qml"));
 
 #ifdef Q_WS_X11
     viewer.showExpanded();
