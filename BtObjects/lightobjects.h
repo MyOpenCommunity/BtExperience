d6086588dc5d2160b9b7ec680aa7ee2cea319be8
Gianni Valdambrini <aleister@develer.com>
2012-02-15 12:36:43 +0100

    Rindentati file che per qualche strano motivo non erano stati modificati per avere i tab invece che gli spazi come da coding style.

--- BtObjects/lightobjects.h
+++ BtObjects/lightobjects.h
@@ -12,82 +12,82 @@ class DimmerDevice;
 
 class LightInterface : public ObjectInterface
 {
-    Q_OBJECT
-    Q_PROPERTY(bool status READ getStatus WRITE setStatus NOTIFY statusChanged)
+	Q_OBJECT
+	Q_PROPERTY(bool status READ getStatus WRITE setStatus NOTIFY statusChanged)
 
 public:
-    // the status of the object: on or off.
-    virtual bool getStatus() const = 0;
-    virtual void setStatus(bool st) = 0;
+	// the status of the object: on or off.
+	virtual bool getStatus() const = 0;
+	virtual void setStatus(bool st) = 0;
 
 signals:
-    void statusChanged();
+	void statusChanged();
 };
 
 
 class Light : public LightInterface
 {
-    Q_OBJECT
+	Q_OBJECT
 
 public:
-    Light(QString name, QString key, LightingDevice *d);
+	Light(QString name, QString key, LightingDevice *d);
 
-    virtual int getObjectId() const
-    {
-        return ObjectInterface::IdLight;
-    }
+	virtual int getObjectId() const
+	{
+		return ObjectInterface::IdLight;
+	}
 
-    virtual QString getObjectKey() const;
+	virtual QString getObjectKey() const;
 
-    virtual ObjectCategory getCategory() const
-    {
-        return ObjectInterface::Lighting;
-    }
+	virtual ObjectCategory getCategory() const
+	{
+		return ObjectInterface::Lighting;
+	}
 
-    virtual QString getName() const;
-    virtual bool getStatus() const;
-    virtual void setStatus(bool st);
+	virtual QString getName() const;
+	virtual bool getStatus() const;
+	virtual void setStatus(bool st);
 
 protected slots:
-    virtual void valueReceived(const DeviceValues &values_list);
+	virtual void valueReceived(const DeviceValues &values_list);
 
 protected:
-    QString name;
-    QString key;
-    bool status;
+	QString name;
+	QString key;
+	bool status;
 
 private:
-    LightingDevice *dev;
+	LightingDevice *dev;
 };
 
 
 class Dimmer : public Light
 {
-    Q_OBJECT
-    Q_PROPERTY(int percentage READ getPercentage WRITE setPercentage NOTIFY percentageChanged)
+	Q_OBJECT
+	Q_PROPERTY(int percentage READ getPercentage WRITE setPercentage NOTIFY percentageChanged)
 
 public:
-    Dimmer(QString name, QString key, DimmerDevice *d);
+	Dimmer(QString name, QString key, DimmerDevice *d);
 
-    virtual int getObjectId() const
-    {
-        return ObjectInterface::IdDimmer;
-    }
+	virtual int getObjectId() const
+	{
+		return ObjectInterface::IdDimmer;
+	}
 
-    virtual int getPercentage() const;
-    virtual void setPercentage(int val);
+	virtual int getPercentage() const;
+	virtual void setPercentage(int val);
 
 signals:
-    void percentageChanged();
+	void percentageChanged();
 
 protected slots:
-    virtual void valueReceived(const DeviceValues &values_list);
+	virtual void valueReceived(const DeviceValues &values_list);
 
 protected:
-    int percentage;
+	int percentage;
 
 private:
-    DimmerDevice *dev;
+	DimmerDevice *dev;
 };
 
 
--- BtObjects/thermalobjects.h
+++ BtObjects/thermalobjects.h
@@ -26,300 +26,300 @@ typedef QHash<int, QVariant> ThermalRegulationState;
 
 class ThermalControlUnit : public ObjectInterface
 {
-    Q_OBJECT
-    Q_ENUMS(SeasonType)
-    Q_ENUMS(ThermalControlUnitId)
-    Q_PROPERTY(SeasonType season READ getSeason WRITE setSeason NOTIFY seasonChanged)
-    Q_PROPERTY(ObjectListModel *modalities READ getModalities NOTIFY modalitiesChanged)
-    Q_PROPERTY(QObject *currentModality READ getCurrentModality NOTIFY currentModalityChanged)
+	Q_OBJECT
+	Q_ENUMS(SeasonType)
+	Q_ENUMS(ThermalControlUnitId)
+	Q_PROPERTY(SeasonType season READ getSeason WRITE setSeason NOTIFY seasonChanged)
+	Q_PROPERTY(ObjectListModel *modalities READ getModalities NOTIFY modalitiesChanged)
+	Q_PROPERTY(QObject *currentModality READ getCurrentModality NOTIFY currentModalityChanged)
 
 public:
-    enum ThermalControlUnitId
-    {
-        IdHoliday,
-        IdOff,
-        IdAntifreeze,
-        IdManual,
-        IdWeeklyPrograms,
-        IdVacation,
-        IdScenarios
-    };
+	enum ThermalControlUnitId
+	{
+		IdHoliday,
+		IdOff,
+		IdAntifreeze,
+		IdManual,
+		IdWeeklyPrograms,
+		IdVacation,
+		IdScenarios
+	};
 
-    enum SeasonType
-    {
-        Summer,
-        Winter
-    };
+	enum SeasonType
+	{
+		Summer,
+		Winter
+	};
 
-    ThermalControlUnit(QString name, QString key, ThermalDevice *d);
+	ThermalControlUnit(QString name, QString key, ThermalDevice *d);
 
-    virtual QString getObjectKey() const;
+	virtual QString getObjectKey() const;
 
-    virtual ObjectCategory getCategory() const
-    {
-        return ObjectInterface::ThermalRegulation;
-    }
+	virtual ObjectCategory getCategory() const
+	{
+		return ObjectInterface::ThermalRegulation;
+	}
 
-    virtual QString getName() const;
+	virtual QString getName() const;
 
-    SeasonType getSeason() const;
-    void setSeason(SeasonType s);
+	SeasonType getSeason() const;
+	void setSeason(SeasonType s);
 
-    ThermalRegulationProgramList getPrograms() const;
+	ThermalRegulationProgramList getPrograms() const;
 
-    ObjectListModel *getModalities() const;
+	ObjectListModel *getModalities() const;
 
-    QObject* getCurrentModality() const;
+	QObject* getCurrentModality() const;
 
 signals:
-    void seasonChanged();
-    void modalitiesChanged();
-    void currentModalityChanged();
+	void seasonChanged();
+	void modalitiesChanged();
+	void currentModalityChanged();
 
 protected slots:
-    virtual void valueReceived(const DeviceValues &values_list);
+	virtual void valueReceived(const DeviceValues &values_list);
 
 protected:
-    QList<ThermalControlUnitObject*> objs;
+	QList<ThermalControlUnitObject*> objs;
 
 private:
-    QString name;
-    QString key;
-    int temperature;
-    SeasonType season;
-    ThermalRegulationProgramList programs;
-    ThermalDevice *dev;
-    int current_modality;
+	QString name;
+	QString key;
+	int temperature;
+	SeasonType season;
+	ThermalRegulationProgramList programs;
+	ThermalDevice *dev;
+	int current_modality;
 };
 
 
 class ThermalControlUnit4Zones : public ThermalControlUnit
 {
-    Q_OBJECT
+	Q_OBJECT
 
 public:
-    ThermalControlUnit4Zones(QString name, QString key, ThermalDevice4Zones *d);
+	ThermalControlUnit4Zones(QString name, QString key, ThermalDevice4Zones *d);
 
-    virtual int getObjectId() const
-    {
-        return ObjectInterface::IdThermalControlUnit4;
-    }
+	virtual int getObjectId() const
+	{
+		return ObjectInterface::IdThermalControlUnit4;
+	}
 
 private:
-    ThermalDevice4Zones *dev;
+	ThermalDevice4Zones *dev;
 };
 
 
 class ThermalControlUnit99Zones : public ThermalControlUnit
 {
-    Q_OBJECT
+	Q_OBJECT
 
 public:
-    ThermalControlUnit99Zones(QString name, QString key, ThermalDevice99Zones *d);
+	ThermalControlUnit99Zones(QString name, QString key, ThermalDevice99Zones *d);
 
-    virtual int getObjectId() const
-    {
-        return ObjectInterface::IdThermalControlUnit99;
-    }
+	virtual int getObjectId() const
+	{
+		return ObjectInterface::IdThermalControlUnit99;
+	}
 
-    ThermalRegulationProgramList getScenarios() const;
+	ThermalRegulationProgramList getScenarios() const;
 
 private:
-    ThermalDevice99Zones *dev;
-    ThermalRegulationProgramList scenarios;
+	ThermalDevice99Zones *dev;
+	ThermalRegulationProgramList scenarios;
 };
 
 
 class ThermalControlUnitObject : public ObjectInterface
 {
-    Q_OBJECT
+	Q_OBJECT
 
 public:
-    ThermalControlUnitObject(QString name, ThermalDevice *dev);
+	ThermalControlUnitObject(QString name, ThermalDevice *dev);
 
-    virtual QString getObjectKey() const;
+	virtual QString getObjectKey() const;
 
-    virtual ObjectCategory getCategory() const
-    {
-        return ObjectInterface::ThermalRegulation;
-    }
+	virtual ObjectCategory getCategory() const
+	{
+		return ObjectInterface::ThermalRegulation;
+	}
 
-    virtual QString getName() const;
+	virtual QString getName() const;
 
 public slots:
-    virtual void apply() = 0;
-    virtual void reset();
+	virtual void apply() = 0;
+	virtual void reset();
 
 protected:
-    ThermalRegulationState current, to_apply;
+	ThermalRegulationState current, to_apply;
 
 protected:
-    ThermalDevice *dev;
-    QString name;
+	ThermalDevice *dev;
+	QString name;
 };
 
 
 class ThermalControlUnitProgram : public ThermalControlUnitObject
 {
-    Q_OBJECT
-    Q_PROPERTY(int programIndex READ getProgramIndex WRITE setProgramIndex NOTIFY programChanged)
-    Q_PROPERTY(int programCount READ getProgramCount)
-    Q_PROPERTY(int programId READ getProgramId NOTIFY programChanged)
-    Q_PROPERTY(QString programDescription READ getProgramDescription NOTIFY programChanged)
+	Q_OBJECT
+	Q_PROPERTY(int programIndex READ getProgramIndex WRITE setProgramIndex NOTIFY programChanged)
+	Q_PROPERTY(int programCount READ getProgramCount)
+	Q_PROPERTY(int programId READ getProgramId NOTIFY programChanged)
+	Q_PROPERTY(QString programDescription READ getProgramDescription NOTIFY programChanged)
 
 public:
-    ThermalControlUnitProgram(QString name, int object_id, const ThermalControlUnit *unit, ThermalDevice *dev);
+	ThermalControlUnitProgram(QString name, int object_id, const ThermalControlUnit *unit, ThermalDevice *dev);
 
-    virtual int getObjectId() const
-    {
-        return object_id;
-    }
+	virtual int getObjectId() const
+	{
+		return object_id;
+	}
 
-    int getProgramCount() const;
+	int getProgramCount() const;
 
-    int getProgramIndex() const;
-    void setProgramIndex(int index);
+	int getProgramIndex() const;
+	void setProgramIndex(int index);
 
-    int getProgramId() const;
-    QString getProgramDescription() const;
+	int getProgramId() const;
+	QString getProgramDescription() const;
 
 public slots:
-    virtual void apply();
+	virtual void apply();
 
 signals:
-    void programChanged();
+	void programChanged();
 
 protected slots:
-    void valueReceived(const DeviceValues &values_list);
+	void valueReceived(const DeviceValues &values_list);
 
 private:
-    ThermalRegulationProgramList programs;
-    int object_id;
+	ThermalRegulationProgramList programs;
+	int object_id;
 };
 
 
 class ThermalControlUnitTimedProgram : public ThermalControlUnitProgram
 {
-    Q_OBJECT
-    Q_PROPERTY(QDate date READ getDate WRITE setDate NOTIFY dateChanged)
-    Q_PROPERTY(QTime time READ getTime WRITE setTime NOTIFY timeChanged)
+	Q_OBJECT
+	Q_PROPERTY(QDate date READ getDate WRITE setDate NOTIFY dateChanged)
+	Q_PROPERTY(QTime time READ getTime WRITE setTime NOTIFY timeChanged)
 
 public:
-    ThermalControlUnitTimedProgram(QString name, int object_id, const ThermalControlUnit *unit, ThermalDevice *dev);
+	ThermalControlUnitTimedProgram(QString name, int object_id, const ThermalControlUnit *unit, ThermalDevice *dev);
 
-    QDate getDate() const;
-    void setDate(QDate date);
+	QDate getDate() const;
+	void setDate(QDate date);
 
-    QTime getTime() const;
-    void setTime(QTime time);
+	QTime getTime() const;
+	void setTime(QTime time);
 
 public slots:
-    virtual void apply();
+	virtual void apply();
 
 signals:
-    void dateChanged();
-    void timeChanged();
+	void dateChanged();
+	void timeChanged();
 };
 
 
 class ThermalControlUnitOff : public ThermalControlUnitObject
 {
-    Q_OBJECT
+	Q_OBJECT
 
 public:
-    ThermalControlUnitOff(QString name, ThermalDevice *dev);
+	ThermalControlUnitOff(QString name, ThermalDevice *dev);
 
-    virtual int getObjectId() const
-    {
-        return ThermalControlUnit::IdOff;
-    }
+	virtual int getObjectId() const
+	{
+		return ThermalControlUnit::IdOff;
+	}
 
 public slots:
-    virtual void apply();
+	virtual void apply();
 };
 
 
 class ThermalControlUnitAntifreeze : public ThermalControlUnitObject
 {
-    Q_OBJECT
+	Q_OBJECT
 
 public:
-    ThermalControlUnitAntifreeze(QString name, ThermalDevice *dev);
+	ThermalControlUnitAntifreeze(QString name, ThermalDevice *dev);
 
-    virtual int getObjectId() const
-    {
-        return ThermalControlUnit::IdAntifreeze;
-    }
+	virtual int getObjectId() const
+	{
+		return ThermalControlUnit::IdAntifreeze;
+	}
 
 public slots:
-    virtual void apply();
+	virtual void apply();
 };
 
 
 class ThermalControlUnitManual : public ThermalControlUnitObject
 {
-    Q_OBJECT
-    Q_PROPERTY(int temperature READ getTemperature WRITE setTemperature NOTIFY temperatureChanged)
+	Q_OBJECT
+	Q_PROPERTY(int temperature READ getTemperature WRITE setTemperature NOTIFY temperatureChanged)
 
 public:
-    ThermalControlUnitManual(QString name, ThermalDevice *dev);
+	ThermalControlUnitManual(QString name, ThermalDevice *dev);
 
-    virtual int getObjectId() const
-    {
-        return ThermalControlUnit::IdManual;
-    }
+	virtual int getObjectId() const
+	{
+		return ThermalControlUnit::IdManual;
+	}
 
-    int getTemperature() const;
-    void setTemperature(int temp);
+	int getTemperature() const;
+	void setTemperature(int temp);
 
 
 public slots:
-    virtual void apply();
+	virtual void apply();
 
 signals:
-    void temperatureChanged();
+	void temperatureChanged();
 
 protected slots:
-    void valueReceived(const DeviceValues &values_list);
+	void valueReceived(const DeviceValues &values_list);
 };
 
 
 class ThermalControlUnitScenario : public ThermalControlUnitObject
 {
-    Q_OBJECT
-    Q_PROPERTY(int scenarioIndex READ getScenarioIndex WRITE setScenarioIndex NOTIFY scenarioChanged)
-    Q_PROPERTY(int scenarioCount READ getScenarioCount)
-    Q_PROPERTY(int scenarioId READ getScenarioId NOTIFY scenarioChanged)
-    Q_PROPERTY(QString scenarioDescription READ getScenarioDescription NOTIFY scenarioChanged)
+	Q_OBJECT
+	Q_PROPERTY(int scenarioIndex READ getScenarioIndex WRITE setScenarioIndex NOTIFY scenarioChanged)
+	Q_PROPERTY(int scenarioCount READ getScenarioCount)
+	Q_PROPERTY(int scenarioId READ getScenarioId NOTIFY scenarioChanged)
+	Q_PROPERTY(QString scenarioDescription READ getScenarioDescription NOTIFY scenarioChanged)
 
 public:
-    ThermalControlUnitScenario(QString name, const ThermalControlUnit99Zones *unit, ThermalDevice99Zones *dev);
+	ThermalControlUnitScenario(QString name, const ThermalControlUnit99Zones *unit, ThermalDevice99Zones *dev);
 
-    virtual int getObjectId() const
-    {
-        return ThermalControlUnit::IdScenarios;
-    }
+	virtual int getObjectId() const
+	{
+		return ThermalControlUnit::IdScenarios;
+	}
 
-    int getScenarioCount() const;
+	int getScenarioCount() const;
 
-    int getScenarioIndex() const;
-    void setScenarioIndex(int index);
+	int getScenarioIndex() const;
+	void setScenarioIndex(int index);
 
-    int getScenarioId() const;
-    QString getScenarioDescription() const;
+	int getScenarioId() const;
+	QString getScenarioDescription() const;
 
 public slots:
-    virtual void apply();
+	virtual void apply();
 
 signals:
-    void scenarioChanged();
+	void scenarioChanged();
 
 protected slots:
-    void valueReceived(const DeviceValues &values_list);
+	void valueReceived(const DeviceValues &values_list);
 
 private:
-    ThermalDevice99Zones *dev;
-    ThermalRegulationProgramList scenarios;
+	ThermalDevice99Zones *dev;
+	ThermalRegulationProgramList scenarios;
 };
 
 
