ade1ab1bf02b7e34e930367eb6a4df8258c64da4
Mattia Barbon <mattia@develer.com>
2012-03-19 18:43:33 +0100

    Aggiunta documentazione per gli oggetti della diffusione sonora.

--- BtObjects/mediaobjects.h
+++ BtObjects/mediaobjects.h
@@ -14,8 +14,6 @@ class SourceBase;
 class PowerAmplifierDevice;
 
 
-// ambients and amplifiers have the area number as the key
-
 QList<ObjectInterface *> createSoundDiffusionSystem(const QDomNode &xml_node);
 
 // internal class
@@ -39,12 +37,28 @@ private:
 };
 
 
+/*!
+	\ingroup SoundDiffusion
+	\brief Properties for a single sound diffusion area
+
+	The object id is \a ObjectInterface::IdMultiChannelSoundAmbient and area number is the object key
+*/
 class SoundAmbient : public SoundAmbientBase
 {
 	friend class TestSoundAmbient;
 
 	Q_OBJECT
+
+	/*!
+		\brief Amplifier status for the area
+
+		Returns true if there is at least one amplifier turned on in this area
+	*/
 	Q_PROPERTY(bool hasActiveAmplifier READ getHasActiveAmplifier NOTIFY activeAmplifierChanged)
+
+	/*!
+		\brief Current sound diffusion source for the area
+	*/
 	Q_PROPERTY(QObject *currentSource READ getCurrentSource NOTIFY currentSourceChanged)
 
 public:
@@ -80,6 +94,12 @@ private:
 };
 
 
+/*!
+	\ingroup SoundDiffusion
+	\brief Properties for the general sound diffusion area
+
+	The object id is \a ObjectInterface::IdMultiChannelGeneralAmbient, object key is empty
+*/
 class SoundGeneralAmbient : public SoundAmbientBase
 {
 	Q_OBJECT
@@ -96,15 +116,35 @@ public:
 };
 
 
-// internal class
+/*!
+	\ingroup SoundDiffusion
+	\brief Base class for sound diffusion sources
+
+	The object id is \a ObjectInterface::IdSoundSource, object key is empty
+*/
 class SourceBase : public ObjectInterface
 {
 	friend class TestSourceBase;
 	friend class TestSoundAmbient;
 
 	Q_OBJECT
+
+	/*!
+		\brief Gets the active status of the source
+
+		The value is true if the source is active in at least one of the areas,
+		false otherwise.
+	*/
 	Q_PROPERTY(bool active READ isActive NOTIFY activeChanged)
+
+	/*!
+		\brief Gets the list of areas that use this source
+	*/
 	Q_PROPERTY(QList<int> activeAreas READ getActiveAreas NOTIFY activeAreasChanged)
+
+	/*!
+		\brief Gets the current track playing on this source
+	*/
 	Q_PROPERTY(int currentTrack READ getCurrentTrack NOTIFY currentTrackChanged)
 
 public:
@@ -130,8 +170,19 @@ public:
 	int getCurrentTrack() const;
 
 public slots:
+	/*!
+		\brief Activates this source on the specified area
+	*/
 	void setActive(int area);
+
+	/*!
+		\brief Go to the previous track (memorized station for the radio)
+	*/
 	void previousTrack();
+
+	/*!
+		\brief Go to the next track (memorized station for the radio)
+	*/
 	void nextTrack();
 
 signals:
@@ -153,6 +204,12 @@ private:
 };
 
 
+/*!
+	\brief Manages an AUX source
+
+	Can be used to control an aux adapter (for ananlogic sound input) or another
+	BTicino touch screen device.
+*/
 class SourceAux : public SourceBase
 {
 	Q_OBJECT
@@ -162,13 +219,30 @@ public:
 };
 
 
+/*!
+	\brief Manages an RDS radio device
+*/
 class SourceRadio : public SourceBase
 {
 	friend class TestSourceRadio;
 
 	Q_OBJECT
+
+	/*!
+		\brief Sets and gets the current memorized station
+
+		The value can be set to 1-5 to listen to one of the memorized stations.
+	*/
 	Q_PROPERTY(int currentStation READ getCurrentStation WRITE setCurrentStation NOTIFY currentStationChanged)
+
+	/*!
+		\brief Gets the currently tuned frquency (in MHz * 100)
+	*/
 	Q_PROPERTY(int currentFrequency READ getCurrentFrequency NOTIFY currentFrequencyChanged)
+
+	/*!
+		\brief Gets the current RDS text
+	*/
 	Q_PROPERTY(QString rdsText READ getRdsText NOTIFY rdsTextChanged)
 
 public:
@@ -182,19 +256,46 @@ public:
 	QString getRdsText() const;
 
 public slots:
+	/*!
+		\brief Go to the previous memorized station
+	*/
 	void previousStation();
+
+	/*!
+		\brief Go to the next memorized station
+	*/
 	void nextStation();
+
+	/*!
+		\brief Save current frequency to the given \a station
+	*/
 	void saveStation(int station);
 
 	void startRdsUpdates();
 	void stopRdsUpdates();
 
-	// changes frequency by steps * 0.05 MHz
+	/*!
+		\brief Increment frequency by \a steps * 0.05 MHz
+	*/
 	void frequencyUp(int steps);
+
+	/*!
+		\brief Decrement frequency by \a steps * 0.05 MHz
+	*/
 	void frequencyDown(int steps);
 
-	// automatic scan up/down; sets frequency to -1 during scanning
+	/*!
+		\brief Automatic upward scan
+
+		Sets frequency to -1 during the scan
+	*/
 	void searchUp();
+
+	/*!
+		\brief Automatic upward scan
+
+		Sets frequency to -1 during the scan
+	*/
 	void searchDown();
 
 signals:
@@ -214,13 +315,27 @@ private:
 };
 
 
+/*!
+	\ingroup SoundDiffusion
+	\brief Manages a sound diffusion amplifier
+
+	The object id is \a ObjectInterface::IdSoundAmplifier and area number is the object key
+*/
 class Amplifier : public ObjectInterface
 {
 	friend class TestAmplifier;
 	friend class TestSoundAmbient;
 
 	Q_OBJECT
+
+	/*!
+		\brief Sets or gets the on/off status of the amplifier
+	*/
 	Q_PROPERTY(bool active READ isActive WRITE setActive NOTIFY activeChanged)
+
+	/*!
+		\brief Sets or gets the amplifier volume (1-31)
+	*/
 	Q_PROPERTY(int volume READ getVolume WRITE setVolume NOTIFY volumeChanged)
 
 public:
@@ -264,6 +379,11 @@ private:
 };
 
 
+/*!
+	\brief container for a power amplifier preset
+
+	The preset number is in the object id, the preset name in the object name.
+*/
 class PowerAmplifierPreset : public ObjectInterface
 {
 	Q_OBJECT
@@ -285,17 +405,54 @@ private:
 };
 
 
+/*!
+	\ingroup SoundDiffusion
+	\brief Manages a sound diffusion power amplifier
+
+	The object id is \a ObjectInterface::IdPowerAmplifier and area number is the object key
+*/
 class PowerAmplifier : public Amplifier
 {
 	friend class TestPowerAmplifier;
 
 	Q_OBJECT
+
+	/*!
+		\brief Gets bass equalization (-10 to +10)
+	*/
 	Q_PROPERTY(int bass READ getBass NOTIFY bassChanged)
+
+	/*!
+		\brief Gets treble equalization (-10 to +10)
+	*/
 	Q_PROPERTY(int treble READ getTreble NOTIFY trebleChanged)
+
+	/*!
+		\brief Gets left/right balance (-10 is full left, +10 is full right)
+	*/
 	Q_PROPERTY(int balance READ getBalance NOTIFY balanceChanged)
+
+	/*!
+		\brief Sets and gets currently selected preset (0 to 19)
+	*/
 	Q_PROPERTY(int preset READ getPreset WRITE setPreset NOTIFY presetChanged)
+
+	/*!
+		\brief Gets currently selected preset description
+	*/
 	Q_PROPERTY(QString presetDescription READ getPresetDescription NOTIFY presetDescriptionChanged)
+
+	/*!
+		\brief Sets and gets loudness status
+	*/
 	Q_PROPERTY(bool loud READ getLoud WRITE setLoud NOTIFY loudChanged)
+
+	/*!
+		\brief Gets the list of available presets
+
+		Returns a list of \a PowerAmplifierPreset objects; the first 10
+		are fixed presets, the last 10 are user-defined
+	*/
 	Q_PROPERTY(ObjectListModel *presets READ getPresets CONSTANT)
 
 public:
@@ -315,16 +472,44 @@ public:
 	void setLoud(bool loud);
 
 public slots:
+	/*!
+		\brief Decrease bass equalization.
+	*/
 	void bassDown();
+
+	/*!
+		\brief Increase bass equalization.
+	*/
 	void bassUp();
 
+	/*!
+		\brief Decrease treble equalization.
+	*/
 	void trebleDown();
+
+	/*!
+		\brief Increase treble equalization.
+	*/
 	void trebleUp();
 
+	/*!
+		\brief Move balance to the left
+	*/
 	void balanceLeft();
+
+	/*!
+		\brief Move balance to the right
+	*/
 	void balanceRight();
 
+	/*!
+		\brief Select previous preset
+	*/
 	void previousPreset();
+
+	/*!
+		\brief Select next preset
+	*/
 	void nextPreset();
 
 signals:
