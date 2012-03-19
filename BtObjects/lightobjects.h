a2c22753de7c8dc0fd1b9ff33cf2c22f1b1f8450
Mattia Barbon <mattia@develer.com>
2012-03-19 17:59:12 +0100

    Aggiunta documentazione per oggetti delle luci.

--- BtObjects/lightobjects.h
+++ BtObjects/lightobjects.h
@@ -10,11 +10,23 @@ class LightingDevice;
 class DimmerDevice;
 
 
+/*!
+	\ingroup Lighting
+	\brief Manages light actuators
+
+	Can also be used to control dimmer 10 and dimmer 100 actuators.
+
+	The object id is \a ObjectInterface::IdLight, the key is the SCS where.
+*/
 class Light : public ObjectInterface
 {
 	friend class TestLight;
 
 	Q_OBJECT
+
+	/*!
+		\brief Sets and gets the on/off status of the light
+	*/
 	Q_PROPERTY(bool status READ getStatus WRITE setStatus NOTIFY statusChanged)
 
 public:
@@ -52,12 +64,27 @@ private:
 };
 
 
+/*!
+	\ingroup Lighting
+	\brief Manages dimmer 10 and dimmer 100 actuators
+
+	Dimmer 100 actuators are controlled as if they were dimmer 10, so there
+	is no way to use fine-grained increments or to control the speed.
+
+	The object id is \a ObjectInterface::IdDimmer, the object key is the SCS where.
+*/
 class Dimmer : public Light
 {
 	friend class TestDimmer;
 	friend class TestDimmer100;
 
 	Q_OBJECT
+
+	/*!
+		\brief Gets the dimmer level on a 1-100 scale
+
+		Note that the level keeps its value even when the dimmer is off
+	*/
 	Q_PROPERTY(int percentage READ getPercentage NOTIFY percentageChanged)
 
 public:
@@ -71,7 +98,14 @@ public:
 	virtual int getPercentage() const;
 
 public slots:
+	/*!
+		\brief Increase dimmer level by about 10%
+	*/
 	void increaseLevel();
+
+	/*!
+		\brief Decrease dimmer level by about 10%
+	*/
 	void decreaseLevel();
 
 signals:
