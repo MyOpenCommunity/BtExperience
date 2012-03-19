9487ac555a6d2d39504044406c5b5c294761e9ed
Mattia Barbon <mattia@develer.com>
2012-03-19 17:59:33 +0100

    Aggiunta documentazione per l'oggetto sonda controllata.

--- BtObjects/thermalprobes.h
+++ BtObjects/thermalprobes.h
@@ -10,34 +10,58 @@
 class ControlledProbeDevice;
 
 
+/*!
+	\ingroup ThermalRegulation
+	\brief Manages thermal regulation controlled probes
+
+	The object id is \a ObjectInterface::IdThermalControlledProbe, the object key is the SCS where.
+*/
 class ThermalControlledProbe : public ObjectInterface
 {
 	friend class TestThermalProbes;
 
 	Q_OBJECT
+
+	/*!
+		\brief Sets or gets the status of the probe
+	*/
 	Q_PROPERTY(ProbeStatus probeStatus READ getProbeStatus WRITE setProbeStatus NOTIFY probeStatusChanged)
+
+	/*!
+		\brief Gets the temperature measured by the probe
+	*/
 	Q_PROPERTY(int temperature READ getTemperature NOTIFY temperatureChanged)
+
+	/*!
+		\brief Gets the target temperature for the probe (for manual mode)
+	*/
 	Q_PROPERTY(int setpoint READ getSetpoint WRITE setSetpoint NOTIFY setpointChanged)
+
+	/*!
+		\brief Sets or gets the fancoil status (only for probes associated with a fancoil)
+	*/
 	Q_PROPERTY(FancoilSpeed fancoil READ getFancoil WRITE setFancoil NOTIFY fancoilChanged)
+
 	Q_ENUMS(ProbeStatus)
 	Q_ENUMS(FancoilSpeed)
 
 public:
 	enum ProbeStatus
 	{
-		Unknown,
-		Manual,
-		Auto,
-		Off,
-		Antifreeze
+		Unknown,    /*!< No state received yet (only during initialization). */
+		Manual,     /*!< Manual mode. */
+		Auto,       /*!< Automatic mode. */
+		Off,        /*!< Zone off. */
+		Antifreeze  /*!< Antifreeze mode. */
 	};
 
 	enum FancoilSpeed
 	{
-		FancoilMin = 1, // this values are the same of the ControlledProbeDevice
-		FancoilMed,
-		FancoilMax,
-		FancoilAuto
+		// these values are the same of the ControlledProbeDevice
+		FancoilMin = 1,  /*!< Minimum speed */
+		FancoilMed,      /*!< Medium speed */
+		FancoilMax,      /*!< Maximum speed */
+		FancoilAuto      /*!< Automatic speed */
 	};
 
 	ThermalControlledProbe(QString name, QString key, ControlledProbeDevice *d);
