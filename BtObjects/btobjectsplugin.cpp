09300a28ed57865714e2c5c43f9e618efd0fa458
Luca Ottaviano <lottaviano@develer.com>
2012-04-04 11:50:40 +0200

    Esportati gli enum di FileObject

--- BtObjects/btobjectsplugin.cpp
+++ BtObjects/btobjectsplugin.cpp
@@ -150,6 +150,8 @@ void BtObjectsPlugin::registerTypes(const char *uri)
 		"unable to create a HardwareSettings instance");
 	qmlRegisterUncreatableType<AntintrusionAlarm>(uri, 1, 0, "AntintrusionAlarm",
 		"unable to create an AntintrusionAlarm instance");
+	qmlRegisterUncreatableType<FileObject>(uri, 1, 0, "FileObject",
+		"unable to create an FileObject instance");
 }
 
 Q_EXPORT_PLUGIN2(BtObjects, BtObjectsPlugin)
