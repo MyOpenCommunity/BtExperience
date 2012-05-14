6b02b2740a9ea29e071790fa9b576f8d56821885
Roberto Gianassi <roberto@develer.com>
2012-05-14 14:02:29 +0200

    Added a comment to EventManager.js module.

--- gui/skins/default/Components/EventManager.js
+++ gui/skins/default/Components/EventManager.js
@@ -4,9 +4,34 @@
 
 Qt.include("../js/logging.js")
 
+
+/**
+  * Logic for screensaver timeout management.
+  *
+  * For screensaver timeout there are two possible scenarios:
+  *     1) the user interacts with the application
+  *     2) an internal application event causes the timeout to reset
+  *
+  * Case #1 is managed by lastTimePress (is computed by C++ layer and
+  *     passed to elapsed function)
+  * Case #2 cannot be mananged in C++ layer, so an updateLast function is needed
+  *
+  * The updateLast function must be called inside QML layer whenever the timeout
+  * must be reset.
+  * An example scenario may be the following:
+  *     1) the screensaver is active
+  *     2) an alarm arrives
+  *     3) the screensaver must disappear to make the user see the alarm
+  *     4) if user doesn't take action, the screensaver must reactivate as soon
+  *         as the timeout elapses
+  *
+  * in such a case, when the alarm arrives, we must call the updateLast function
+  * so the scenario described is automatically managed.
+  *
+  */
+
 // time of last meaningful event, used to activate the screensaver
 var last = 0
-
 
 // updates "manually" last
 function updateLast() {
