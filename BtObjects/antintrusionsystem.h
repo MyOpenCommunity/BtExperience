11cd994bac680fda19872c92db854010df3ae6fb
Mattia Barbon <mattia@develer.com>
2012-05-31 15:11:21 +0200

    Aggiunta documentazione di base per il sistema dell'antintrusione.

--- BtObjects/antintrusionsystem.h
+++ BtObjects/antintrusionsystem.h
@@ -25,9 +25,20 @@ QList<ObjectPair> parseAntintrusionScenario(const QDomNode &obj, const UiiMapper
 AntintrusionSystem *createAntintrusionSystem(QList<AntintrusionZone *> zones, QList<AntintrusionAlarmSource *> aux, QList<AntintrusionScenario *> scenarios);
 
 
+/*!
+	\brief The source for an alarm (either a zone or an aux input)
+
+	The \c name property holds the zone/aux description (if any)
+*/
 class AntintrusionAlarmSource : public ObjectInterface
 {
 	Q_OBJECT
+
+	/*!
+		\brief The zone or aux number.
+
+		Can be 1-8 for configured zones, 9-16 for special zones and 1-16 for aux inputs.
+	*/
 	Q_PROPERTY(int number READ getNumber CONSTANT)
 
 public:
@@ -50,9 +61,22 @@ private:
 };
 
 
+/*!
+	\brief An antintrusion zone
+
+	In addition to the properties for a generic alarm source, antintrusion zones can be
+	partialized (alarms will not be triggered/reported for partialized zones).
+*/
 class AntintrusionZone : public AntintrusionAlarmSource
 {
 	Q_OBJECT
+
+	/*!
+		\brief When a zone is partialized the system does not report alarms for this zone
+
+		After setting desired zone status on all antintrusion zones, call
+		\a requestPartialization() or \a toggleActivation() to transmit the setting to the control unit.
+	*/
 	Q_PROPERTY(bool partialization READ getPartialization WRITE setPartialization NOTIFY partializationChanged)
 
 public:
@@ -75,10 +99,26 @@ private:
 };
 
 
+/*!
+	\brief A set of antintrusion zones that can be enabled together
+*/
 class AntintrusionScenario : public ObjectInterface
 {
 	Q_OBJECT
+
+	/*!
+		\brief Whether this scenario is active
+
+		This property becomes \c true when all the zones in this scenario are active
+		(not partialized).
+
+		Note that this reflects the zone status in the program, and is not synchronized
+		with the control unit unless \a requestPartialization() or \a toggleActivation()
+		is called.
+	*/
 	Q_PROPERTY(bool selected READ isSelected NOTIFY selectionChanged)
+
+	/// Description for the scenario
 	Q_PROPERTY(QString description READ getDescription CONSTANT)
 
 public:
@@ -92,7 +132,12 @@ public:
 	// return the description of the scenario, used by the omonymous role
 	Q_INVOKABLE QString getDescription() const;
 
-	// apply the scenario
+	/*!
+		\brief Applies the scenario
+
+		After applying the scenario, call \a requestPartialization() or
+		\a toggleActivation() to transmit the setting to the control unit.
+	*/
 	Q_INVOKABLE void apply();
 
 	// check if the scenario is selected
@@ -111,13 +156,49 @@ private:
 };
 
 
+/*!
+	\brief A single antintrusion alarm
+*/
 class AntintrusionAlarm : public ObjectInterface
 {
 	Q_OBJECT
+
+	/*!
+		\brief The alarm type
+
+		\sa AlarmType
+	*/
 	Q_PROPERTY(AlarmType type READ getType CONSTANT)
+
+	/*!
+		\brief Alarm source id
+
+		For alarms coming from a normal antintrusion zone (\c Intrusion, some \c Tampering alarms)
+		this is the antintrusion zone number (1-8) and \c name is set to the zone name.
+
+		For alarms coming from an auxiliary input (\c Technical) this is the input id (1-16)
+		and \c name is set to the description of the input.
+
+		For alarms coming from a special zone (\c Tampering) this is the special zone number
+		(9-16) and \c name is empty.
+
+		For \c Antipanic alarms, this is the fixed value 9 and \c name is empty.
+	*/
 	Q_PROPERTY(int number READ getNumber CONSTANT)
+
+	/*!
+		\brief The source of the alarm
+
+		Only set for \c Technical, \c Intrusion and \c Tamper alarms coming from a normal
+		antintrusion zone (1-8).
+	*/
 	Q_PROPERTY(ObjectInterface *source READ getSource CONSTANT)
+
+	/*!
+		\brief The time at which the alarm has been received by the UI
+	*/
 	Q_PROPERTY(QDateTime date_time READ getDateTime CONSTANT)
+
 	Q_ENUMS(AlarmType)
 
 public:
@@ -152,15 +233,28 @@ private:
 };
 
 
+/*!
+	\brief Antintrusion control unit object
+*/
 class AntintrusionSystem : public ObjectInterface
 {
 friend class TestAntintrusionSystem;
 
 	Q_OBJECT
+
+	/// List of configured zones (contains \c AntintrusionZone objects)
 	Q_PROPERTY(ObjectDataModel *zones READ getZones CONSTANT)
+
+	/// List of configured scenarios (contains \c AntintrusionScenario objects)
 	Q_PROPERTY(ObjectDataModel *scenarios READ getScenarios CONSTANT)
+
+	/// List of active alarms (contains \c AntintrusionAlarm objects)
 	Q_PROPERTY(ObjectDataModel *alarms READ getAlarms NOTIFY alarmsChanged)
+
+	/// Returns \c true when the antintrusion system is active
 	Q_PROPERTY(bool status READ getStatus NOTIFY statusChanged)
+
+	/// The currently-selected antintrusion scenario
 	Q_PROPERTY(QObject *currentScenario READ getCurrentScenario NOTIFY currentScenarioChanged)
 
 public:
@@ -180,7 +274,19 @@ public:
 	ObjectDataModel *getScenarios() const;
 	ObjectDataModel *getAlarms() const;
 
+	/*!
+		\brief Apply the current partialization state to the control unit
+
+		Only call it when the antintrusion system is active, otherwise call
+		\a toggleActivation() that activates the system and sets the partialization.
+	*/
 	Q_INVOKABLE void requestPartialization(const QString &password);
+
+	/*!
+		\brief Toggle activation status for the system
+
+		When activating the system, it also sets the current partialization status
+	*/
 	Q_INVOKABLE void toggleActivation(const QString &password);
 
 	bool getStatus() const
