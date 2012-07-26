a23298f9c8b986f2570e5bc760fd6fd4d4dbe714
Roberto Gianassi <roberto@develer.com>
2012-07-26 10:19:08 +0200

    A more specific logging message in case of parsing errors.
    Signed-off-by: Roberto Gianassi <roberto@develer.com>

--- BtObjects/btobjectsplugin.cpp
+++ BtObjects/btobjectsplugin.cpp
@@ -93,9 +93,14 @@ namespace
 
 BtObjectsPlugin::BtObjectsPlugin(QObject *parent) : QDeclarativeExtensionPlugin(parent)
 {
+	// for logging
+	QString errorMsg;
+	int errorLine, errorColumn;
 	QFile fh(QFileInfo(QDir(qApp->applicationDirPath()), CONF_FILE).absoluteFilePath());
-	if (!fh.exists() || !document.setContent(&fh))
-		qFatal("The config file %s does not seem a valid xml configuration file", qPrintable(QFileInfo(fh).absoluteFilePath()));
+	if (!fh.exists() || !document.setContent(&fh, &errorMsg, &errorLine, &errorColumn)) {
+		QString msg = QString("The config file %1 does not seem a valid xml configuration file: Error description: %2, line: %3, column: %4").arg(qPrintable(QFileInfo(fh).absoluteFilePath())).arg(errorMsg).arg(errorLine).arg(errorColumn);
+		qFatal(qPrintable(msg));
+	}
 
 	bt_global::config = new QHash<GlobalField, QString>();
 	(*bt_global::config)[TS_NUMBER] = QString::number(0);
@@ -420,10 +425,16 @@ void BtObjectsPlugin::createObjectsFakeConfig(QDomDocument document)
 
 void BtObjectsPlugin::parseConfig()
 {
+	// for logging
+	QString errorMsg;
+	int errorLine, errorColumn;
 	QFile fh(QFileInfo(QDir(qApp->applicationDirPath()), LAYOUT_FILE).absoluteFilePath());
 	QDomDocument document;
-	if (!fh.exists() || !document.setContent(&fh))
-		qFatal("The layout file %s does not seem a valid xml configuration file", qPrintable(QFileInfo(fh).absoluteFilePath()));
+	if (!fh.exists() || !document.setContent(&fh, &errorMsg, &errorLine, &errorColumn)) {
+		QString msg = QString("The config file %1 does not seem a valid xml configuration file: Error description: %2, line: %3, column: %4").arg(qPrintable(QFileInfo(fh).absoluteFilePath())).arg(errorMsg).arg(errorLine).arg(errorColumn);
+		qFatal(qPrintable(msg));
+	}
+
 	foreach (const QDomNode &container, getChildren(document.documentElement(), "container"))
 	{
 		int container_id = getIntAttribute(container, "id");
