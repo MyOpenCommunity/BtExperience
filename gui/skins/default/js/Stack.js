MyOpenCommunity/BtExperience

commit 2a08563d53968db9d4d0b06027d58017cacec1a5

the page skipper now supports the properties
Signed-off-by: Gianni Valdambrini <aleister@develer.com>

gui/skins/default/js/Stack.js
@@ -303,7 +303,7 @@ function _findTargetPage(filename, properties) {
         if (skipper_component.status === 1) {
             logDebug("Found page skipper: " + _skipperFilename(filename))
             // the skipper is present and ready, use it
-            var skipper = skipper_component.createObject(null)
+            var skipper = skipper_component.createObject(null, properties)
             if (skipper === null) {
                 logWarning("Could not create skipper object for page: " + filename)
                 // terminate the loop anyway
@@ -313,7 +313,8 @@ function _findTargetPage(filename, properties) {
             var ret = skipper.pageSkip()
             filename = ret["page"]
             deletingObjects.push(skipper)
-            properties = ret["properties"]
+            if (filename !== "")
+                properties = ret["properties"]
         }
         else {
             // terminate the loop

commit 3d0846ada71d723870fbb9ec6d7102c54b6986f2

Fixed a bug introduced with commit [2a08563]
Signed-off-by: Gianni Valdambrini <aleister@develer.com>

gui/skins/default/js/Stack.js
@@ -303,18 +303,19 @@ function _findTargetPage(filename, properties) {
         if (skipper_component.status === 1) {
             logDebug("Found page skipper: " + _skipperFilename(filename))
             // the skipper is present and ready, use it
-            var skipper = skipper_component.createObject(null, properties)
+            var skipper = skipper_component.createObject(null, properties === undefined ? {} : properties)
             if (skipper === null) {
                 logWarning("Could not create skipper object for page: " + filename)
                 // terminate the loop anyway
                 filename = ""
             }
-
-            var ret = skipper.pageSkip()
-            filename = ret["page"]
-            deletingObjects.push(skipper)
-            if (filename !== "")
-                properties = ret["properties"]
+            else {
+                var ret = skipper.pageSkip()
+                filename = ret["page"]
+                deletingObjects.push(skipper)
+                if (filename !== "")
+                    properties = ret["properties"]
+            }
         }
         else {
             // terminate the loop