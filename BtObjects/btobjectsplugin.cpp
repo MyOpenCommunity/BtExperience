4ae5818b5038ac52fd1fa757950a36286254f721
Gianni Valdambrini <aleister@develer.com>
2012-10-05 11:50:22 +0200

    Revert "Remove the family from the objectKey."
    This reverts commit bab479d5fa9742d4d1954c718250a1eb9800774b.
    
    E' necessario mantenere l'object key sulla famiglia perche' viene
    utilizzato nella pagina del dettaglio come filtro.
    
    Signed-off-by: Gianni Valdambrini <aleister@develer.com>

--- BtObjects/btobjectsplugin.cpp
+++ BtObjects/btobjectsplugin.cpp
@@ -294,6 +294,8 @@ void BtObjectsPlugin::createObjects(QDomDocument document)
 	QHash<int, QPair<QDomNode, QDomNode> > probe4zones, splitcommands;
 	QDomNode cu99zones;
 
+	int energy_family = 1;
+
 	foreach (const QDomNode &xml_obj, getChildren(document.documentElement(), "obj"))
 	{
 		QList<ObjectPair> obj_list;
@@ -422,8 +424,9 @@ void BtObjectsPlugin::createObjects(QDomDocument document)
 			obj_list = parseLoadWithoutCU(xml_obj);
 			break;
 		case ObjectInterface::IdEnergyData:
-			objmodel << new EnergyFamily(getAttribute(xml_obj, "descr"));
-			obj_list = parseEnergyData(xml_obj);
+			objmodel << new EnergyFamily(getAttribute(xml_obj, "descr"), QString::number(energy_family));
+			obj_list = parseEnergyData(xml_obj, QString::number(energy_family));
+			++energy_family;
 			break;
 
 		case ObjectInterface::IdSimpleScenario:
