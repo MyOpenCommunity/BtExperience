10a42504f7f28537c3e570e3e6de5a76262f4e30
Mattia Barbon <mattia@develer.com>
2012-11-30 18:48:06 +0100

    Add basic documentation for calibration class.
    Signed-off-by: Mattia Barbon <mattia@develer.com>

--- BtObjects/calibration.h
+++ BtObjects/calibration.h
@@ -6,6 +6,14 @@
 #include <QWSPointerCalibrationData>
 
 
+/*!
+	\brief Set calibration points
+
+	To perform calibration, call \ref startCalibration() to put the screen
+	in raw mode.  Ask the user to click the 4 corners and the center of the screen
+	and set the coordinates using \ref setCalibrationPoint().  If \ref applyCalibration()
+	returns \c false, repeat the procedure.
+*/
 class Calibration : public QObject
 {
 	Q_OBJECT
@@ -28,10 +36,34 @@ public:
 	Calibration(QObject *parent = 0);
 
 public slots:
+	/*!
+		\brief Put the screen in raw mode and save a backup of the calibration file
+	*/
 	void startCalibration();
+
+	/*!
+		\brief Set the screen and raw coordinates for one point.
+		\param screen the screen coordinate where the user should have clicked
+		\param touch the raw coordinates from the mouse event
+	*/
 	void setCalibrationPoint(Point point, QPoint screen, QPoint touch);
+
+	/*!
+		\brief Apply the calibration
+
+		Before applying the calibration, the function performs a simple sanity check
+		of the coordinates.
+	*/
 	bool applyCalibration();
+
+	/*!
+		\brief Restores the calibration saved by \ref startCalibration()
+	*/
 	void resetCalibration();
+
+	/*!
+		\brief Deletes the backup file created by \ref startCalibration()
+	*/
 	void saveCalibration();
 
 private:
