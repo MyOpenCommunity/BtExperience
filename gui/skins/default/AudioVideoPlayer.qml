MyOpenCommunity/BtExperience

commit c0c95a53ad6b262ea9936cc93fe89787557d36cf

Use LoaderWithProps for popups in BasePage.
Signed-off-by: Luca Ottaviano <lottaviano@develer.com>

gui/skins/default/BasePage.qml
@@ -20,10 +20,8 @@ Image {
 
     // The alert management and API.
     function showAlert(sourceElement, message) {
-        popupLoader.sourceComponent = alertComponent
+        popupLoader.setComponent(alertComponent, {"message": message, "source": sourceElement})
         popupLoader.item.closeAlert.connect(closeAlert)
-        popupLoader.item.message = message
-        popupLoader.item.source = sourceElement
         page.state = "alert"
     }
 
@@ -42,8 +40,8 @@ Image {
     // may be destroyed before user input finished, it will not work.
     // Example: create a popup from a MenuColumn that immediately after is
     // destroyed.
-    function installPopup(sourceComponent) {
-        popupLoader.sourceComponent = sourceComponent
+    function installPopup(sourceComponent, properties) {
+        popupLoader.setComponent(sourceComponent, properties)
         popupLoader.item.closePopup.connect(closePopup)
         page.state = "popup"
     }
@@ -119,7 +117,7 @@ Image {
         anchors.fill: parent
         z: 10
 
-        Loader {
+        LoaderWithProps {
             id: popupLoader
             opacity: 0
             anchors.verticalCenter: parent.verticalCenter
@@ -133,7 +131,7 @@ Image {
 
     function closePopup() {
         page.state = ""
-        popupLoader.sourceComponent = undefined
+        popupLoader.setComponent(undefined)
     }
 
     Constants {

commit 2636a3588ee73b4574a18ea9aa040abf480b8747

Use the installPopup() API everywhere a popup is needed.
This fixes the fallback caused by commit
c0c95a53ad6b262ea9936cc93fe89787557d36cf

Signed-off-by: Luca Ottaviano <lottaviano@develer.com>

gui/skins/default/Antintrusion.qml
@@ -13,12 +13,7 @@ SystemPage {
 
     // KeyPad management and API
     function showKeyPad(title, errorMessage, okMessage) {
-        popupLoader.sourceComponent = keypadComponent
-        popupLoader.item.cancelClicked.connect(closeKeyPad)
-        popupLoader.item.mainLabel = title
-        popupLoader.item.errorLabel = errorMessage
-        popupLoader.item.okLabel = okMessage
-        antintrusion.state = "popup"
+        installPopup(keypadComponent, {"mainLabel": title, "errorLabel": errorMessage, "okLabel": okMessage})
     }
 
     function closeKeyPad() {

gui/skins/default/AudioVideoPlayer.qml
@@ -636,11 +636,11 @@ Page {
                 global.audioState.disableState(AudioState.LocalPlaybackMute)
         }
         onLoopDetected: {
-            player.installPopup(errorFeedback)
+            var props = {text: qsTr("Loop detected")}
             if (player.model === undefined) // ip radio
-                player.popupLoader.item.text = qsTr("No tunable web radio")
-            else // general case
-                player.popupLoader.item.text = qsTr("Loop detected")
+                props = {text: qsTr("No tunable web radio")}
+
+            player.installPopup(errorFeedback, props)
         }
     }
 

gui/skins/default/BasePage.qml
@@ -35,6 +35,13 @@ Image {
         }
     }
 
+    Connections {
+        id: popupConnection
+        target: null
+        onClosePopup: closePopup()
+        ignoreUnknownSignals: true
+    }
+
     // Warning: please note that popupLoader doesn't take ownership of the
     // component that you have created. If you try to install a component which
     // may be destroyed before user input finished, it will not work.
@@ -42,7 +49,7 @@ Image {
     // destroyed.
     function installPopup(sourceComponent, properties) {
         popupLoader.setComponent(sourceComponent, properties)
-        popupLoader.item.closePopup.connect(closePopup)
+        popupConnection.target = popupLoader.item
         page.state = "popup"
     }
 

gui/skins/default/Components/KeyPad.qml
@@ -12,12 +12,14 @@ SvgImage {
     property string textInserted
 
     signal cancelClicked
+    signal closePopup
     signal digitClicked(string digit)
 
     source: "../images/common/panel_keypad.svg"
 
     onCancelClicked: {
         textInserted = ""
+        keypad.closePopup()
     }
 
     onDigitClicked: {

gui/skins/default/Components/LoaderWithProps.qml
@@ -7,9 +7,8 @@ Item {
     function setComponent(component, properties) {
         properties = properties || {}
         if (component === undefined) {
-            itemLoader.width = 0
-            itemLoader.height = 0
-            item.destroy()
+            if (item !== null)
+                item.destroy()
             item = null
         }
         else {

gui/skins/default/IntercomPage.qml
@@ -27,10 +27,7 @@ BasePage {
     }
 
     Component.onCompleted: {
-        popupLoader.sourceComponent = popupComponent
-        state = "popup"
-        popupLoader.item.dataObject = page.callObject
-        popupLoader.item.state = "callFrom"
+        installPopup(popupComponent, {dataObject: page.callObject, state: "callFrom"})
         popupLoader.item.dataObject.callEnded.connect(popupLoader.item.callEndedCallBack)
     }
 }

gui/skins/default/PopupPage.qml
@@ -40,10 +40,7 @@ BasePage {
         NumberAnimation { duration: constants.alertTransitionDuration }
     }
 
-    Component.onCompleted: {
-        popupLoader.sourceComponent = popupComponent
-        state = "popup"
-    }
+    Component.onCompleted: installPopup(popupComponent)
 
     function updateUnreadMessages(unreadMessages) {
         privateProps.update(PopupLogic.updateUnreadMessages(unreadMessages))
@@ -130,22 +127,18 @@ BasePage {
             }
 
             if (data === "Antintrusion") {
-                popupLoader.sourceComponent = undefined
                 Stack.goToPage("Antintrusion.qml", {"navigationTarget": Navigation.ALARM_LOG})
             }
 
             if (data === "Supervision") {
-                popupLoader.sourceComponent = undefined
                 Stack.goToPage("EnergyManagement.qml", {"navigationTarget": Navigation.SUPERVISION})
             }
 
             if (data === "Messages") {
-                popupLoader.sourceComponent = undefined
                 Stack.goToPage("Messages.qml")
             }
 
             if (data === "GlobalView") {
-                popupLoader.sourceComponent = undefined
                 Stack.goToPage("EnergyGlobalView.qml")
             }
         }
@@ -172,7 +165,6 @@ BasePage {
         }
 
         function closePopup() {
-            popupLoader.sourceComponent = undefined
             Stack.popPage()
         }
     }

gui/skins/default/Profile.qml
@@ -190,8 +190,7 @@ Page {
         }
 
         function showEditBox(favorite) {
-            installPopup(popup)
-            popupLoader.item.favoriteItem = favorite.itemObject
+            installPopup(popup, {favoriteItem: favorite.itemObject})
         }
 
         function addNote() {