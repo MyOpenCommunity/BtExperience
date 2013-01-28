MyOpenCommunity/BtExperience

commit 1fb8d77e9109be7f7864245123f8370d6484a18c

Bug #19003, fix some texts that overflow their container.
Signed-off-by: Luca Ottaviano <lottaviano@develer.com>

gui/skins/default/Components/NavigationBar.qml
@@ -69,6 +69,7 @@ Item {
         id: label
         color: "#343434"
         anchors.centerIn: background
+        width: background.height - 10 // little margin
         rotation: 270
         font.pixelSize: 32
     }

gui/skins/default/AddQuicklink.qml
@@ -212,7 +212,9 @@ Page {
             elide: Text.ElideMiddle
             anchors {
                 left: linkBgImage.left
-                leftMargin: bg.width / 100 * 3.92
+                leftMargin: bg.width / 100 * 1.5
+                right: linkBgImage.right
+                rightMargin: bg.width / 100 * 1.5
                 verticalCenter: linkBgImage.verticalCenter
             }
         }
@@ -245,7 +247,9 @@ Page {
             elide: Text.ElideMiddle
             anchors {
                 left: nameBgImage.left
-                leftMargin: bg.width / 100 * 3.92
+                leftMargin: bg.width / 100 * 1.5
+                right: nameBgImage.right
+                rightMargin: bg.width / 100 * 1.5
                 verticalCenter: nameBgImage.verticalCenter
             }
         }

gui/skins/default/Components/ControlLeftRight.qml
@@ -16,6 +16,8 @@ SvgImage {
         anchors {
             verticalCenter: rightButton.verticalCenter
             left: parent.left
+            right: leftButton.left
+            rightMargin: 4
             leftMargin: 7
         }
         font.pixelSize: 15
@@ -24,6 +26,7 @@ SvgImage {
     }
 
     ButtonImageThreeStates {
+        id: leftButton
         defaultImageBg: "../images/common/btn_frecce.svg"
         pressedImageBg: "../images/common/btn_frecce_P.svg"
         shadowImage: "../images/common/ombra_btn_frecce.svg"

gui/skins/default/Components/MenuTitle.qml
@@ -23,6 +23,8 @@ SvgImage {
         anchors {
             left: parent.left
             leftMargin: parent.width / 100 * 5
+            right: parent.right
+            rightMargin: parent.width / 100 * 5
             verticalCenter: parent.verticalCenter
         }
     }

gui/skins/default/Components/Text/UbuntuLightText.qml
@@ -9,4 +9,5 @@ Text {
         else
             return ""
     }
+    elide: Text.ElideRight
 }

gui/skins/default/Components/Text/UbuntuMediumText.qml
@@ -9,4 +9,5 @@ Text {
         else
             return ""
     }
+    elide: Text.ElideRight
 }

gui/skins/default/Rooms.qml
@@ -87,9 +87,17 @@ Page {
             source: index === floorView.currentIndex ? "images/common/pianoS.png" : "images/common/piano.png"
             UbuntuLightText {
                 text: itemObject.description
-                anchors.horizontalCenter: parent.horizontalCenter
-                anchors.verticalCenter: parent.verticalCenter
-                color: index === floorView.currentIndex === true ? "white" : "black"
+                anchors {
+                    // the image is a png and it has borders, so I'm using
+                    // fixed values here.
+                    left: parent.left
+                    leftMargin: 14
+                    right: parent.right
+                    rightMargin: 14
+                    verticalCenter: parent.verticalCenter
+                }
+                horizontalAlignment: Text.AlignHCenter
+                color: index === floorView.currentIndex ? "white" : "black"
             }
 
             BeepingMouseArea {

commit 326dbab1ba5c1ba1b2c147b7477ef275790546fd

Fix bug with short texts in navigation bar, introduced with commit 1fb8d77e
The text item is transformed relative to the original position of the text, not
relative to the size of the item.
This is fine until the size of the element is more than the size of the text, as
the commit above did. With this configuration, centering is done relative to
the Item, while the rotation is done relative to the text, resulting in text
that was drawn outside the screen.
Fixed by using an Item as a positioner, then drawing the text inside it.

Signed-off-by: Luca Ottaviano <lottaviano@develer.com>

gui/skins/default/Components/NavigationBar.qml
@@ -65,12 +65,26 @@ Item {
         }
     }
 
-    UbuntuLightText {
-        id: label
-        color: "#343434"
+    // This Item is only a positioner element for the text. Text is rotated
+    // relative to the original position of the text (excluding alignment etc)
+    // but centering and anchoring is done relative to the size of the Item
+    // class (which is a parent of Text element).
+    // Using an Item for positioning makes it easier to get it right.
+    Item {
+        // swap width <-> height because we are rotating, anchors.fill is not
+        // going to work
+        width: background.height
+        height: background.width
         anchors.centerIn: background
-        width: background.height - 10 // little margin
         rotation: 270
-        font.pixelSize: 32
+
+        UbuntuLightText {
+            id: label
+            anchors.fill: parent
+            color: "#343434"
+            horizontalAlignment: Text.AlignHCenter
+            verticalAlignment: Text.AlignVCenter
+            font.pixelSize: 32
+        }
     }
 }