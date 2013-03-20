MyOpenCommunity/BtExperience

commit 10b6479fa0e18c2fd2ceb13fd96dfa688b7cdd0d

Some style fixes (review).
Signed-off-by: Luca Ottaviano <lottaviano@develer.com>

gui/skins/default/Components/EnergyManagement/LoadDiagnostic.qml
@@ -28,8 +28,7 @@ MenuColumn {
 
             currentIndex: -1
             onCurrentPageChanged: element.closeChild()
-            delegate: MenuItem {
-                property variant itemObject: listModel.getObject(index)
+            delegate: MenuItemDelegate {
                 clickable: false
                 status: MenuItem.status(itemObject)
                 hasChild: MenuItem.hasChild(itemObject)

BtObjects/vct.h
@@ -22,7 +22,6 @@ QList<ObjectPair> parseVdeCamera(const QDomNode &xml_node);
 QList<ObjectPair> parseInternalIntercom(const QDomNode &xml_node);
 QList<ObjectPair> parseExternalIntercom(const QDomNode &xml_node);
 QList<ObjectPair> parseSwitchboard(const QDomNode &xml_node);
-QList<ObjectPair> parseStaircaseLight(const QDomNode &xml_node);
 QList<ObjectPair> parsePager(const QDomNode &xml_node);
 
 ObjectInterface *createCCTV(QList<ObjectPair> places);

commit 44257ccf55d30a9c7fede7dde572457351c65889

Fix a regression in load diagnostic introduced in 10b6479fa0e18c2fd2ceb13fd96dfa688b7cdd0d
Signed-off-by: Luca Ottaviano <lottaviano@develer.com>

gui/skins/default/Components/EnergyManagement/LoadDiagnostic.qml
@@ -30,6 +30,7 @@ MenuColumn {
             onCurrentPageChanged: element.closeChild()
             delegate: MenuItemDelegate {
                 clickable: false
+                itemObject: listModel.getObject(index)
                 status: MenuItem.status(itemObject)
                 hasChild: MenuItem.hasChild(itemObject)
                 description: MenuItem.description(itemObject)